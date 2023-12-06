e52a8a533f78e3c635988f7c3e93797bc2762d2c
Justin Hall <justin.r.hall@gmail.com>
2023-12-06 14:58:31 -0700

    test: ignore coverage of .js files

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,10 @@
-import { defineConfig } from 'vitest/config';
+import { defineConfig, configDefaults } from 'vitest/config';
 
 export default defineConfig({
   test: {
     coverage: {
       reporter: ['text', 'lcov', 'clover'],
+      exclude: [...(configDefaults.coverage.exclude ?? []), '**/*.js'],
     },
     globals: true,
     environment: 'node',
