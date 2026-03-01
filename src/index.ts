a94abef15a518590fdf3f68087cae20c609080ad
Justin Hall <justin.r.hall@gmail.com>
2026-02-28 22:44:54 -0700

    chore: reformat

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,20 @@
 export function booleanize(input: boolean | number | bigint | string | null | undefined): boolean {
   if (input === null) return false;
-  if (typeof input === 'undefined') return false;
-  if (typeof input === 'boolean') return input;
-  if (typeof input === 'number') return Boolean(input);
-  if (typeof input === 'bigint') return Boolean(input);
-  if (typeof input === 'string') {
+  if (typeof input === "undefined") return false;
+  if (typeof input === "boolean") return input;
+  if (typeof input === "number") return Boolean(input);
+  if (typeof input === "bigint") return Boolean(input);
+  if (typeof input === "string") {
     if (input.length === 0) return false;
 
     const inputStr = input.toLowerCase();
 
     switch (inputStr) {
-      case 'null':
-      case 'undefined':
-      case 'false':
-      case 'no':
-      case '0':
+      case "null":
+      case "undefined":
+      case "false":
+      case "no":
+      case "0":
         return false;
       default:
         return true;
